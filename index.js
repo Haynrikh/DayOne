ee69d3febf5eaa0a5890816f7d8fbd6ac579500e
Haynrikh <haynrikh.santrosyan@optimumpartners.co>
2022-03-28 13:19:03 +0400

    testing done

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require("express");
-const app = express();
-const port = 9000;
-const db = require("./queries");
 
+require("dotenv").config();
+
+const app = express();
+const port = process.env.SERVER;
+const funcs = require("./queries");
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  response.json({ info: "Node.js, Express, and Postgres API" });
-});
+app.get("/notes", funcs.getNotes);
+app.get("/notes/:id", funcs.getUserById);
+app.post("/notes", funcs.createNote);
+app.put("/notes/:id", funcs.updateNote);
+app.delete("/notes/:id", funcs.deleteNote);
 
-app.get("/notes", db.getNotes);
-app.post("/notes", db.createNote);
-app.put("/notes", db.updateNote);
-app.delete("/notes", db.deleteNote);
-
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+module.exports = server;
--- queries.js
+++ queries.js
@@ -1,63 +1,94 @@
-const req = require("express/lib/request");
-
 const Pool = require("pg").Pool;
+require("dotenv").config();
+const { DB_USER, DB_HOST, DB_DATABASE, DB_PASSWORD } = require("./configs");
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "users",
-  password: "mypass",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_BASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
+
 const getNotes = async (req, res) => {
-  pool.query("SELECT * FROM todo", async (error, results) => {
+  await pool.query("SELECT * FROM todo", (error, results) => {
     if (error) {
       throw error;
     }
-    await res.status(200).json(results.rows);
+    res.status(200).json(results.rows);
   });
 };
 
+const getUserById = async (request, response) => {
+  const id = parseInt(request.params.id);
+  if (!isNaN(id) && id > 0) {
+    await pool.query(
+      "SELECT * FROM todo WHERE id = $1",
+      [id],
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        response.status(200).json(results.rows);
+      }
+    );
+  } else {
+    res.send(404).send("ID not found");
+  }
+};
+
 const createNote = async (req, res) => {
-  const { id, title, done } = req.body;
-  pool.query(
-    "INSERT INTO todo (id, title, done) VALUES ($1, $2, $3)",
-    [id, title, done],
-    async (error, response) => {
+  const { title, done } = req.body;
+  await pool.query(
+    "INSERT INTO todo (title, done) VALUES ($1, $2)",
+    [title, done],
+    (error, results) => {
       if (error) {
         throw error;
-      } else {
-        await res.status(201).send(`Note added`);
       }
+      res.status(201).send("Note created");
     }
   );
 };
 
 const updateNote = async (req, res) => {
-  const id = req.body.id;
-  const { title, done } = req.body;
-  pool.query(
-    "UPDATE todo SET title = $1, done = $2 WHERE id = $3",
-    [title, done, id],
-    async (error, results) => {
-      if (error) {
-        throw error;
+  const id = parseInt(req.params.id);
+  if (!isNaN(id) && id > 0) {
+    const { title, done } = req.body;
+    await pool.query(
+      "UPDATE todo SET title = $1, done = $2 WHERE id = $3",
+      [title, done, id],
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        res.status(200).send(`Note modified with ID: ${id}`);
       }
-      await res.status(200).send(`Note modified`);
-    }
-  );
+    );
+  } else {
+    res.send(404).send("ID not found");
+  }
 };
 const deleteNote = async (req, res) => {
-  const id = req.body.id;
-  pool.query("DELETE FROM todo WHERE id = $1", [id], async (error, results) => {
-    if (error) {
-      throw error;
-    }
-    await res.status(200).send(`Note deleted with ID: ${id}`);
-  });
+  const id = parseInt(req.params.id);
+  if (!isNaN(id) && id > 0) {
+    await pool.query(
+      "DELETE FROM todo WHERE id = $1",
+      [id],
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        res.status(200).send(`Note deleted with ID: ${id}`);
+      }
+    );
+  } else {
+    res.send(404).send("ID not found");
+  }
 };
 
 module.exports = {
   getNotes,
+  getUserById,
   createNote,
   updateNote,
   deleteNote,
